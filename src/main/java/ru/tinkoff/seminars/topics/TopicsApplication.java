22a98f9fad877ca0258b7fe2a7613f6745fcd8e2
s.muntyanov <s.muntyanov@tinkoff.ru>
2024-04-04 21:00:53 +0500

    init commit

---
+++ src/main/java/ru/tinkoff/seminars/topics/TopicsApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.seminars.topics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicsApplication.class, args);
+	}
+
+}
